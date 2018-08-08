7e193f9d25b28f0912d6575c0a2bb4c2025e4402
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-08 13:18:00 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,20 +3,15 @@ require 'pry'
 class PigLatinizer
 
   def piglatinize(str)
-    str = str.split(' ')
-    alpha = ('a'..'z').to_a
-    vowels = %w[a e i o u]
-    consonants = alpha - vowels
-
-    if vowels.include?(str[0])
-    str + 'way'
-    elsif consonants.include?(str[0]) && consonants.include?(str[1])
-    str[2..-1] + str[0..1] + 'way'
-    elsif consonants.include?(str[0])
-    str[1..-1] + str[0] + 'way'
-    else
-    str.join # return unchanged
-    end
+    vowels = %w{a e i o u}
+    sent.gsub(/(\A|\s)\w+/) do |str|
+            str.strip!
+        while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
+            str += str[0]
+            str = str[1..-1]
+        end
+        str  = ' ' + str + 'ay'
+    end.strip
   end
 
   def to_pig_latin(sentence)
