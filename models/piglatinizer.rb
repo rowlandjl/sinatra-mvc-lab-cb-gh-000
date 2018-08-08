72dfeaab8869e244e304d7e0e4b7ddd8520cfef6
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-08 13:36:07 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,7 +17,8 @@ class PigLatinizer
         first = string.shift
         "#{string.join}#{first}ay"
       elsif string.first.downcase.match(/[aeoui]/)
-        "#{string.join}way"
+        first = string.shift
+        "#{string.join}#{}way"
       else
         string.join
       end
