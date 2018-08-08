79845a524c0211cbf6f26f0c2162dfd16762f4d5
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-08 13:13:26 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,25 +3,22 @@ require 'pry'
 class PigLatinizer
 
   def piglatinize(string)
-      string = string.split('')
-      if string.first.downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[1].downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[2].downcase.match(/[bcdfghjklmnpqrstvwxyz]/)
-        first = string.shift
-        second = string.shift
-        third = string.shift
-        "#{string.join}#{first}#{second}#{third}ay"
-      elsif string.first.downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[1].downcase.match(/[bcdfghjklmnpqrstvwxyz]/)
-        first = string.shift
-        second = string.shift
-        "#{string.join}#{first}#{second}ay"
-      elsif string.first.downcase.match(/[bcdfghjklmnpqrstvwxyz]/)
-        first = string.shift
-        "#{string.join}#{first}ay"
-      elsif string.first.downcase.match(/[aeoui]/)
-        "#{string.join}way"
-      else
-        string.join
-      end
-    end
+    def translate str
+alpha = ('a'..'z').to_a
+vowels = %w[a e i o u]
+consonants = alpha - vowels
+
+if vowels.include?(str[0])
+str + 'ay'
+elsif consonants.include?(str[0]) && consonants.include?(str[1])
+str[2..-1] + str[0..1] + 'ay'
+elsif consonants.include?(str[0])
+str[1..-1] + str[0] + 'ay'
+else
+str # return unchanged
+end
+end
+  end
 
   def to_pig_latin(sentence)
     piglatinize(sentence)
